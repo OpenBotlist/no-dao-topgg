86dd16644f8614a02bec6ba85f40bf5faf60e7ef
Ashley <iamashley@tuta.io>
2022-02-12 14:26:01 +0300

    Create disable-web3.js

---
+++ scripts/web3/disable-web3.js
@@ -0,0 +1,7 @@
+/*
+DISABLES WEB3 ON TOP.GG
+for more info about why web3 is not secure:https://www.vox.com/recode/22907072/web3-crypto-nft-bitcoin-metaverse
+*/
+
+var url = "top.gg"
+var https = "https://" + url
